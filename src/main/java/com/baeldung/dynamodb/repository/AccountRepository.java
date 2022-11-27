bf63c48ed39f7d48bcb417c058ecda12d53e2265
Bosco <adalbertobosco@gmail.com>
2022-11-27 09:29:23 -0300

    feat: included integration  tests

---
+++ src/main/java/com/baeldung/dynamodb/repository/AccountRepository.java
@@ -0,0 +1,6 @@
+package com.baeldung.dynamodb.repository;
+
+import com.baeldung.dynamodb.entity.Account;
+
+public class AccountRepository extends AbstractRepository<Account, String> {
+}
---
+++ src/main/java/com/baeldung/service/AccountService.java
@@ -0,0 +1,7 @@
+package com.baeldung.service;
+
+import java.util.Objects;
+// TODO mudar tudo pra estrutura da conta, implementar interface do servico, depois servico
+public class AccountService {
+    
+}
